11cee404f1e35a78a5d2c066a5d364a493e7296c
StrahilKazlachev <strahil.kazlachev@gmail.com>
2016-08-09 21:35:43 +0300

    repro - dynamic navigation with navigationStrategy

--- skeleton-esnext/src/app.js
+++ skeleton-esnext/src/app.js
@@ -4,7 +4,13 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
+      {
+        route: 'nav/:alias', name: 'dynamic', navigationStrategy: instruction => {
+          instruction.config.moduleId = `dynamic-${instruction.params.alias}`;
+          instruction.config.href = instruction.fragment;
+        }
+      }
     ]);
 
     this.router = router;
